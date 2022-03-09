f5d7b46749c01830c23a3ecb67bfcf70cef90f76
Seok chan YUN <seyun@e1r6p3.42.fr>
2022-03-09 02:27:49 +0100

    cpp00

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/08 22:25:51 by seyun             #+#    #+#             */
+/*   Updated: 2022/03/08 23:54:18 by seyun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <cctype>
+
+int main(int ac, char **av)
+{
+	if (ac == 1)
+		std::cout << "* LOUD AND UNBEAREABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		int i = 0;
+		while (av[++i])
+		{
+			int j = -1;
+			while (av[i][++j])
+				std::cout << static_cast<char>(std::toupper(av[i][j]));
+		}
+		std::cout << std::endl;
+	}
+	return (0);
+}
