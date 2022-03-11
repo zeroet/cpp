ae01f63cdedd2c6819daf51a25d4ffbef23d5442
Seok chan YUN <seyun@e1r3p21.42.fr>
2022-03-11 23:14:06 +0100

    ex01

---
+++ cpp00/ex01/PhoneBook.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/11 23:01:44 by seyun             #+#    #+#             */
+/*   Updated: 2022/03/11 23:13:54 by seyun            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "PhoneBook.hpp"
+
+User::User(void)
+{
+	std::cout << "Contructor called" << std::endl;
+}
+
+User::~User(void)
+{
+	std::cout << "Decontructor called" << std::endl;
+}
+
+int	User::Add(void)
+{
+}
