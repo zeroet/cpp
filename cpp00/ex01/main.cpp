c10c601d7a15cb956808ef2eca3959de484c1654
Seok chan YUN <seyun@e1r6p8.42.fr>
2022-03-16 02:25:26 +0100

    저 장 을 잘 하 자

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:01:14 by seyun             #+#    #+#             */
-/*   Updated: 2022/03/15 20:21:51 by seyun            ###   ########.fr       */
+/*   Updated: 2022/03/15 23:44:40 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,8 @@
 int main(void)
 {
 	Phonebook book;
-	std::string str("");
 
-	std::getline(std::cin, str);
-	book.CommandCheck(str);
-	
-	std::cout << book.usr[0].getFirstName() << std::endl;
-	std::cout << book.usr[0].getLastName() << std::endl;
-	std::cout << book.usr[0].getNickName() << std::endl;
-	std::cout << book.usr[0].getPhoneNumber() << std::endl;
-	std::cout << book.usr[0].getDarkestSecret() << std::endl;
+	while(1)
+		book.Command();
 	return (0);
 }
