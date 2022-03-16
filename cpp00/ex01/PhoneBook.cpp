b1310f502b8a50cefae51e4644f5aed4075583fa
Seok chan YUN <seyun@e1r6p11.42.fr>
2022-03-16 18:29:50 +0100

    end 01

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -71,6 +71,26 @@ void Phonebook::Add(void)
 		std::cout << "Phonebook is full" << std::endl;
 }
 
+void Phonebook::RecapInfo(void)
+{
+	int index = getIndex();
+	int	i(0);
+
+	while (i <= index)
+	{
+		std::cout << "|";
+		std::cout << std::setw(10) << i;
+		std::cout << "|";
+		std::cout << std::setw(10) << GetFirstName(i);
+		std::cout << "|";
+		std::cout << std::setw(10) << GetLastName(i);
+		std::cout << "|";
+		std::cout << std::setw(10) << GetNickName(i);
+		std::cout << "|" << std::endl;
+		i++;
+	}
+}
+
 void Phonebook::PutSearchPrint(void)
 {
 	std::cout << "|";
@@ -82,20 +102,79 @@ void Phonebook::PutSearchPrint(void)
 	std::cout << "|";
 	std::cout << std::setw(10) << "nick name";
 	std::cout << "|" << std::endl;
+	RecapInfo();
+}
+
+std::string Phonebook::GetFirstName(int index)
+{
+	if (_usr[index].getFirstName().length() > 10)
+		return (_usr[index].getFirstName().substr(0, 9).append("."));
+	else
+		return (_usr[index].getFirstName());
+}
+
+std::string Phonebook::GetLastName(int index)
+{
+	if (_usr[index].getLastName().length() > 10)
+		return (_usr[index].getLastName().substr(0 , 9).append("."));
+	else
+		return (_usr[index].getLastName());
+}
+
+std::string Phonebook::GetNickName(int index)
+{
+	if (_usr[index].getNickName().length() > 10)
+		return (_usr[index].getNickName().substr(0 , 9).append("."));
+	else
+		return (_usr[index].getNickName());
+}
+
+void Phonebook:: PrintFullInfo(int index)
+{
+	std::cout << "first name: " << _usr[index].getFirstName() << std::endl;
+	std::cout << "last name: " << _usr[index].getLastName() << std::endl;
+	std::cout << "nick name: " << _usr[index].getNickName() << std::endl;
+	std::cout << "phone number: " << _usr[index].getPhoneNumber() << std::endl;
+	std::cout << "darkest secret: " << _usr[index].getDarkestSecret() << std::endl;
+}
+
+bool Phonebook::CheckStr(std::string str)
+{
+	if (str.length() > 2)
+		return (0);
+	if (std::isdigit(str[0]))
+	{
+		if ((stoi(str) >= 0) && (stoi(str) < 8))
+			return (1);
+	}
+	else
+		return (0);
+	return (0);
 }
 
 void Phonebook::Search(void)
 {
 	int index = getIndex();
-	int i(0);
+	int	i(0);
 	std::string str;
 
 	if (index == -1)
 		std::cout << "ADD info First" << std::endl;
 	PutSearchPrint();
-	std::cout << "Enter Index or N" << std::endl;
-	if (std::getline(std::cin, str).eof())
-		Exit();
+	while (1)
+	{
+		std::cout << "Enter Index" << std::endl;
+		if (std::getline(std::cin, str).eof())
+			Exit();
+		else if (CheckStr(str)) 
+		{	
+			i = stoi(str);
+			PrintFullInfo(i);
+			break ;
+		}
+		else
+			std::cout << "Command not found" << std::endl;
+	}
 }
 
 void Phonebook::Exit(void) const
