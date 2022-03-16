459d7f855e822aad681d58a5835e56d953f8b34b
Seok chan YUN <seyun@e1r6p11.42.fr>
2022-03-16 18:29:50 +0100

    end 01

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:02:02 by seyun             #+#    #+#             */
-/*   Updated: 2022/03/16 02:25:08 by seyun            ###   ########.fr       */
+/*   Updated: 2022/03/16 18:11:40 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 #include <iostream>
 #include <string>
 #include <iomanip>
+#include <cctype>
 #include "User.hpp"
 
 class Phonebook {
@@ -30,14 +31,20 @@ public:
 	Phonebook();
 	~Phonebook();
 
-	void	Add(void);
-	void	Search(void);
-	void	Exit(void) const;
-	void	Command(void);
-	void	setIndex(int num);
-	int		getIndex() const;
+	void		Add(void);
+	void		Search(void);
+	void		Command(void);
+	void		setIndex(int num);
+	void		PrintFullInfo(int index);
+	void		PutSearchPrint();
+	void		RecapInfo(void);
+	int			getIndex() const;
+	void		Exit(void) const;
+	bool		CheckStr(std::string str);
+	std::string GetFirstName(int index);
+	std::string GetLastName(int index);
+	std::string GetNickName(int index);
 	std::string CheckValue(std::string question) const;
-	void	PutSearchPrint();
 };
 
 #endif
