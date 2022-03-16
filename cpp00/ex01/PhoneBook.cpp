c81ac6dcc418d5acd83fd7da33a00cceb2128a84
Seok chan YUN <seyun@e1r6p8.42.fr>
2022-03-16 02:25:26 +0100

    저 장 을 잘 하 자

--- cpp00/ex01/PhoneBook.cpp
+++ cpp00/ex01/PhoneBook.cpp
@@ -1,18 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   PhoneBook.cpp                                      :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/11 23:01:44 by seyun             #+#    #+#             */
-/*   Updated: 2022/03/15 20:22:01 by seyun            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "PhoneBook.hpp"
 
-Phonebook::Phonebook() : num_usr(0)
+Phonebook::Phonebook() : _index(-1)
 {
 	std::cout << "Phonebook Open" << std::endl;
 }
@@ -22,50 +10,96 @@ Phonebook::~Phonebook()
 	std::cout << "Phonebook Close" << std::endl;
 }
 
-int	Phonebook::CommandCheck( std::string str)
+void Phonebook::setIndex(int num)
 {
+	this->_index = num;
+}
+
+int	Phonebook::getIndex() const
+{
+	return (this->_index);
+}
+
+void Phonebook::Command()
+{
+	std::string str("");
+	
+	std::cout << "Enter : ADD | SEARCH | EXIT" << std::endl;
+	if(std::getline(std::cin, str).eof())
+		Exit();
 	if (str.compare("ADD") == 0)
 		Add();
-	if (str.compare("Search") == 0)
+	else if (str.compare("SEARCH") == 0)
 		Search();
-	if (str.compare("Exit") == 0)
+	else if (str.compare("EXIT") == 0)
 		Exit();
 	else
-		std::cout << "Error" << std::endl;
-	return (0);
+		std::cout << "Command not found" << std::endl;
 }
 
-int	Phonebook::Add(void)
+std::string Phonebook::CheckValue(std::string question) const
 {
-	str = "";
+	std::string ret;
+
+	while(ret.empty())
+	{
+		std::cout << question << std::endl;
+		if (std::getline(std::cin, ret).eof())
+			Exit();
+		else if (!ret.empty())
+			break ;
+	}
+	return (ret);
+}
+
+void Phonebook::Add(void)
+{
+	std::string str = "";
+	int index = getIndex();
+	
+	if (++index < 8)
+	{
+		_usr[index].setFirstName(CheckValue("first name: "));
+		_usr[index].setLastName(CheckValue("last name: "));
+		_usr[index].setNickName(CheckValue("nick name: "));
+		_usr[index].setPhoneNumber(CheckValue("phone number: "));
+		_usr[index].setDarkestSecret(CheckValue("darkest secret: "));
+		setIndex(index);
+		std::cout << "info saved in Phonebook index[" << index << "]" << std::endl;
+	}
+	else
+		std::cout << "Phonebook is full" << std::endl;
+}
+
+void Phonebook::PutSearchPrint(void)
+{
+	std::cout << "|";
+	std::cout << std::setw(10) << "index";
+	std::cout << "|";
+	std::cout << std::setw(10) << "first name";
+	std::cout << "|";
+	std::cout << std::setw(10) << "last name";
+	std::cout << "|";
+	std::cout << std::setw(10) << "nick name";
+	std::cout << "|" << std::endl;
+}
+
+void Phonebook::Search(void)
+{
+	int index = getIndex();
 	int i(0);
+	std::string str;
 
-	std::cout << "First Name :" << std::endl;
-	std::getline(std::cin,str);
-	usr[i].setFirstName(str);
-	std::cout << "Last Name: " << std::endl;
-	std::getline(std::cin, str);
-	usr[i].setLastName(str);
-	std::cout << "Nick Name: " << std::endl;
-	std::getline(std::cin, str);
-	usr[i].setNickName(str);
-	std::cout << "Phone Number: " << std::endl;
-	std::getline(std::cin, str);
-	usr[i].setPhoneNumber(str);
-	std::cout << "DarkestSecret: " <<std::endl;
-	std::getline(std::cin, str);
-	usr[i].setDarkestSecret(str);
-
-	num_usr++;
-	return (0);
+	if (index == -1)
+		std::cout << "ADD info First" << std::endl;
+	PutSearchPrint();
+	std::cout << "Enter Index or N" << std::endl;
+	if (std::getline(std::cin, str).eof())
+		Exit();
 }
 
-int Phonebook::Search(void)
+void Phonebook::Exit(void) const
 {
-	return (0);
-}
-
-int Phonebook::Exit(void)
-{
-	return (0);
+	Phonebook::~Phonebook();
+	exit(0);
 }
--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: seyun <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 23:02:02 by seyun             #+#    #+#             */
-/*   Updated: 2022/03/15 20:21:58 by seyun            ###   ########.fr       */
+/*   Updated: 2022/03/16 02:25:08 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,29 @@
 
 #include <iostream>
 #include <string>
+#include <iomanip>
 #include "User.hpp"
 
 class Phonebook {
 
 private:
 	
-	std::string _contact[8];
+	User _usr[8];
+	int _index;
 
 public:
 	
 	Phonebook();
 	~Phonebook();
 
-	std::string str;
-
-	User usr[8];
-	
-	int num_usr;
-	int Add(void);
-	int Search(void);
-	int	Exit(void);
-	int CommandCheck(std::string str);
+	void	Add(void);
+	void	Search(void);
+	void	Exit(void) const;
+	void	Command(void);
+	void	setIndex(int num);
+	int		getIndex() const;
+	std::string CheckValue(std::string question) const;
+	void	PutSearchPrint();
 };
 
 #endif
