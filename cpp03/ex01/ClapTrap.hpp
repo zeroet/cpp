f9e5edc0f11824d702ecd6267e6005a59ce1cb79
zeroet <y36609672@yahoo.com>
2022-03-23 23:00:31 +0100

    cpp3 ex01

---
+++ cpp03/ex01/ClapTrap.hpp
@@ -0,0 +1,38 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+#include <iostream>
+#include <string>
+
+# define COLOUR_Y "\033[0;38;5;220m"
+# define COLOUR_G "\033[0;38;5;42m"
+# define COLOUR_R "\033[0;38;5;203m"
+# define COLOUR_B "\033[0;38;5;21m"
+# define COLOUR_FIN "\033[0m"
+
+class ClapTrap {
+
+private:
+	std::string _Name;
+	int _Hit_Point;
+	int _Energy_Point;
+	int _Attack_Point;
+
+public:
+	ClapTrap(void);
+	ClapTrap(std::string str);
+	ClapTrap(const ClapTrap &rhs);
+	ClapTrap& operator=(const ClapTrap &rhs);
+	~ClapTrap();
+
+	void	attack( const std::string &target );
+	void	takeDamage( unsigned int amount );
+	void	beRepaired( unsigned int amout );
+	
+	int	getHit() const;
+	int	getEnergy() const;
+	int	getAttack() const;
+	std::string	getName() const;
+};
+
+#endif
