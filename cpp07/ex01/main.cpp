daa70b2c92f88be6223e5c8b7d349e91f76b6277
zeroet <y36609672@yahoo.com>
2022-04-15 17:29:03 +0200

    08

--- cpp07/ex01/main.cpp
+++ cpp07/ex01/main.cpp
@@ -8,7 +8,7 @@ int main()
 	double arry3[] = {2.123, 3.34312, 42.42, 42222.3222};
 
 	::iter(arry, 3, printArry<std::string>);
-	::iter(arry1, 3, printArry<int>);
+	::iter(arry1, 2, printArry<int>);
 	::iter(arry2, 3, printArry<float>);
 	::iter(arry3, 4, printArry<double>);
 	return (0);
