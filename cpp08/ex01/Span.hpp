6364d913543437c7899a7d40cf43902aa287a5ea
zeroet <y36609672@yahoo.com>
2022-04-16 00:29:41 +0200

    12

---
+++ cpp08/ex01/Span.hpp
@@ -0,0 +1,6 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+
+#include <algorithm>
+#include <iostream>
+#include <string>
---
+++ cpp08/ex01/main.cpp
@@ -0,0 +1,18 @@
+#include "Span.hpp"
+
+# define YELLOW "\033[0;38;5;220m"
+# define GREEN "\033[0;38;5;42m"
+# define RED "\033[0;38;5;196m"
+# define BLUE "\033[0;38;5;21m"
+# define PINK "\033[0;38;5;205m"
+# define PURPLE "\033[0;38;5;93m"
+# define ORANGE "\033[38;5;202m"
+# define FIN "\033[0m"
+
+
+int main(void)
+{
+
+
+	return (0);
+}
